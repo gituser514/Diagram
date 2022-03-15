adf6485da586cad21b982e206643798aee1a2543
gituser514 <92817201+gituser514@users.noreply.github.com>
2022-03-15 16:57:13 -0400

    Create GymIncerts.sql

---
+++ GymIncerts.sql
@@ -0,0 +1,18 @@
+-- inserts
+
+insert into users(
+
+);
+
+insert into receptionists (name, phone_number, email, address)
+values ('John Vanier', 1-514-1111111, 'JohnVanier@someserver.com', 'Monteral');
+insert into receptionists (name, phone_number, email, address)
+values ('Jane Vanier', 1-514-2222222, 'JaneVanier@someserver.com', 'Monteral1');
+insert into receptionists (name, phone_number, email, address)
+values ('Alice Vanier', 1-514-3333333, 'AliceVanier@someserver.com', 'Monteral2');
+insert into supervisors (name, phone_number, email, address)
+values  ('John Supervisor', 1-514-4444444, 'JohnSupervisor@someserver.com', 'Monteral3');
+insert into supervisors (name, phone_number, email, address)
+values  ('Jane Supervisor', 1-514-5555555, 'JaneSupervisor@someserver.com', 'Monteral4');
+insert into supervisors (name, phone_number, email, address)
+values  ('Alice Supervisor', 1-514-6666666, 'AliceSupervisor@someserver.com', 'Monteral5');
