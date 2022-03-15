8f737c1936dd209970e210bfedeb3c5e075e7a3e
gituser514 <92817201+gituser514@users.noreply.github.com>
2022-03-15 17:00:24 -0400

    Update GymIncerts.sql

--- GymIncerts.sql
+++ GymIncerts.sql
@@ -10,9 +10,17 @@ insert into receptionists (name, phone_number, email, address)
 values ('Jane Vanier', 1-514-2222222, 'JaneVanier@someserver.com', 'Monteral1');
 insert into receptionists (name, phone_number, email, address)
 values ('Alice Vanier', 1-514-3333333, 'AliceVanier@someserver.com', 'Monteral2');
+
 insert into supervisors (name, phone_number, email, address)
 values  ('John Supervisor', 1-514-4444444, 'JohnSupervisor@someserver.com', 'Monteral3');
 insert into supervisors (name, phone_number, email, address)
 values  ('Jane Supervisor', 1-514-5555555, 'JaneSupervisor@someserver.com', 'Monteral4');
 insert into supervisors (name, phone_number, email, address)
 values  ('Alice Supervisor', 1-514-6666666, 'AliceSupervisor@someserver.com', 'Monteral5');
+
+insert into trainers (name, phone_number, email, address)
+values ('John Trainer', 1-514-7777777, 'JohnTrainer@someserver.com', 'Monteral6');
+insert into trainers (name, phone_number, email, address)
+values ('Jane Trainer', 1-514-88888888, 'JaneTrainer@someserver.com', 'Monteral7');
+insert into trainers (name, phone_number, email, address)
+values ('Alice Trainer', 1-514-9999999, 'AliceTrainer@someserver.com', 'Monteral8');
