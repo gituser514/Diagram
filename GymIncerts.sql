c947583f55ba1ca19232301e2c5348a338e6fc6f
gituser514 <92817201+gituser514@users.noreply.github.com>
2022-03-16 13:27:42 -0400

    Update GymIncerts.sql

--- GymIncerts.sql
+++ GymIncerts.sql
@@ -24,3 +24,33 @@ insert into trainers (name, phone_number, email, address)
 values ('Jane Trainer', 1-514-88888888, 'JaneTrainer@someserver.com', 'Monteral7');
 insert into trainers (name, phone_number, email, address)
 values ('Alice Trainer', 1-514-9999999, 'AliceTrainer@someserver.com', 'Monteral8');
+
+
+
+
+insert into locker (locker_number, locker_location) VALUES (0002, 'Card');
+insert into locker (locker_number, locker_location) VALUES (0003, 'Card');
+insert into locker (locker_number, locker_location) VALUES (0001, 'Card');
+insert into locker (locker_number, locker_location) VALUES (0004, 'Card');
+insert into locker (locker_number, locker_location) VALUES (0005, 'Card');
+insert into locker (locker_number, locker_location) VALUES (0006, 'Card');
+insert into locker (locker_number, locker_location) VALUES (0007, 'Card');
+insert into locker (locker_number, locker_location) VALUES (0008, 'Card');
+insert into locker (locker_number, locker_location) VALUES (0009, 'Card');
+insert into locker (locker_number, locker_location) VALUES (0010, 'Card');
+insert into locker (locker_number, locker_location) VALUES (0011, 'Card');
+insert into locker (locker_number, locker_location) VALUES (0012, 'Card');
+insert into locker (locker_number, locker_location) VALUES (0013, 'Wght');
+insert into locker (locker_number, locker_location) VALUES (0014, 'Wght');
+insert into locker (locker_number, locker_location) VALUES (0015, 'Wght');
+insert into locker (locker_number, locker_location) VALUES (0016, 'Wght');
+insert into locker (locker_number, locker_location) VALUES (0017, 'Wght');
+insert into locker (locker_number, locker_location) VALUES (0018, 'Wght');
+insert into locker (locker_number, locker_location) VALUES (0019, 'Wght');
+insert into locker (locker_number, locker_location) VALUES (0020, 'Wght');
+insert into locker (locker_number, locker_location) VALUES (0021, 'Wght');
+insert into locker (locker_number, locker_location) VALUES (0022, 'Wght');
+insert into locker (locker_number, locker_location) VALUES (0023, 'Wght');
+insert into locker (locker_number, locker_location) VALUES (0024, 'Wght');
+
+
